355e20abf0fbb727f462147adb2670ed5a7b6cf9
Rob Taylor <Robert_taylor_@hotmail.com>
2012-04-20 10:00:26 +0100

    filter

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,25 @@
 class LocationsController < ApplicationController
 
+before_filter :find_user
+
+before_filter :find_location, :only => [:show, :edit, :update, :destroy]
 
 def index
- @locations = Location.find_by_user_id(params[:user_id])
+
 end
 
 def show
 
+end
+
+private 
+
+def find_user
+ @user = User.find(params[:user_id])
+end
+
+def find_location
+ @location = Location.find(params[:id])
 end
 
 end
